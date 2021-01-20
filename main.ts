fde6bea10e024c9d463e540cbb24182330768de3
Mallozzi27 <77386424+Mallozzi27@users.noreply.github.com>
2021-01-20 10:00:15 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-scene.setBackgroundColor(8)
+tiles.setTilemap(tiles.createTilemap(hex`100010000001030303030e0e0e0e03030303040000060f0f0f0f0f0f0f0f0f0f0f0f050000060f0f0f0f0f0f0f0f0f0f0f0f050000060f0f0f0f0f0f0f0f0f0f0f0f050000060f0f0f0f0f0f0f0f0f0f0f0f050000060f0f0f0f0f0f0f0f0f0f0f0f050000060f0f0f0f0f0f0f0f0f0f0f0f05000007090909020f0f0f0f0a090909080000000000000d0f0f0f0f05000000000000000000000d0f0f0f0f05000000000000010e0e0e0b0f0f0f0f0c0e0e0e0400000d0f0f0f0f0f0f0f0f0f0f0f0f0500000d0f0f0f0f0f0f0f0f0f0f0f0f0500000d0f0f0f0f0f0f0f0f0f0f0f0f0500000d0f0f0f0f0f0f0f0f0f0f0f0f050000070909090909090909090909090800`, img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, [myTiles.transparency16,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.purpleOuterNorth0,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.purpleOuterSouth1,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleInnerSouthEast,sprites.dungeon.purpleInnerSouthWest,sprites.dungeon.purpleOuterWest1,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.floorLight0], TileScale.Sixteen))
